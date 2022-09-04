a183a3c0a7ee42f92cb0943ac7db956a1d1d95c5
ZACK! <mallowthepuff@gmail.com>
2022-09-04 14:18:56 -0500

    implemented PrintJobs

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -10,7 +10,7 @@ public class TechJobs {
 
     static Scanner in = new Scanner(System.in);
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
@@ -112,7 +112,7 @@ public class TechJobs {
                 validChoice = true;
             }
 
-        } while(!validChoice);
+        } while (!validChoice);
 
         return choiceKeys[choiceIdx];
     }
@@ -120,6 +120,18 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.size() == 0) {
+            System.out.println("No Results");
+        } else {
+
+            for (int i = 0; i < someJobs.size(); i++) {
+                System.out.println("\n*****");
+
+                for (Map.Entry<String, String> job: someJobs.get(i).entrySet()) {
+                    System.out.printf(job.getKey() + ": " + job.getValue() + "\n");
+                }
+                System.out.println("*****");
+            }
+        }
     }
 }
